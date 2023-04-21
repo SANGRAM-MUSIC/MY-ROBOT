674a91ab51defe004267cbfca9181edf0657f7a5
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-21 22:57:00 +0530

    Update disasters.py

--- MukeshRobot/modules/disasters.py
+++ MukeshRobot/modules/disasters.py
@@ -629,6 +629,8 @@ __help__ = f"""
 
 `⚠️ ʀᴇᴀᴅ ғʀᴏᴍ ᴛᴏᴘ`
 ᴠɪsɪᴛ [sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ](f"https://t.me{SUPPORT_CHAT}") ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ.
+
+☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](f"https://t.me/the_vip_boy")............☆
 """
 
 SUDO_HANDLER = CommandHandler("addsudo", addsudo)
@@ -661,7 +663,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = " ⍟ ᴅᴇᴠꜱ ⍟"
+__mod_name__ = "♨️ᴅᴇᴠꜱ♨️"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
