2ea6e414f57a2d2298426ce5e4bad467542bdff1
SANGRAM-MUSIC <120346630+SANGRAM-MUSIC@users.noreply.github.com>
2023-04-27 21:30:23 +0530

    Update tagall.py

--- MukeshRobot/modules/tagall.py
+++ MukeshRobot/modules/tagall.py
@@ -62,7 +62,7 @@ async def mentionall(event):
         if not chat_id in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"[{user.first_name}](tg://user?id={usr.id}), "
+        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}), "
         if usrnum == 1:
             if mode == "text_on_cmd":
                 txt = f"{usrtxt} {random.choice(WORDSS)}"
